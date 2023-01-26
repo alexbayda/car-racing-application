b6effcd9e34545ee1abdc04f2002f4fc8668854a
Oleksandr.bayda <oleksandr.bayda@cantire.com>
2023-01-26 19:58:10 +0200

    create raw game

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Application {
+import java.util.ArrayList;
+
+public class Application {
+
+    static ArrayList<Car> cars = new ArrayList<>();
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++){
+            cars.add(new Car("Car " + i, 0));
+        }
+        Race race = new Race(cars);
+        race.startRace();
+    }
+
+
+
 }
--- src/main/java/Race.java
+++ src/main/java/Race.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Race {
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Race {
+    private final ArrayList<Car> cars;
+    private final ExecutorService executor;
+
+    public Race(ArrayList<Car> cars) {
+        this.cars = cars;
+        executor = Executors.newFixedThreadPool(10);
+    }
+
+    public void startRace() {
+        for (Car car : cars) {
+            executor.execute(car);
+        }
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        executor.shutdownNow();
+    }
 }
