348d2094722f1648f58df0e60901b5783094c508
Oleksandr.bayda <oleksandr.bayda@cantire.com>
2023-01-26 19:57:31 +0200

    Initial commit

---
+++ src/main/java/Car.java
@@ -0,0 +1,13 @@
+public class Cars {
+
+    public int speed;
+    public String name;
+
+    public Cars(int speed, String name){
+        this.speed = speed;
+        this.name = name;
+    };
+
+
+
+}
