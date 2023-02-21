85bb9ed183a08c302e86170f8bf748dc738aa77b
Oleksandr.bayda <oleksandr.bayda@cantire.com>
2023-02-21 20:46:13 +0200

    stabilization

--- src/main/java/Athlete.java
+++ src/main/java/Athlete.java
@@ -1,8 +1,6 @@
 import lombok.Getter;
 import lombok.SneakyThrows;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 class Athlete implements Runnable {
     @Getter
     private String name;
@@ -21,12 +19,12 @@ class Athlete implements Runnable {
 
     @SneakyThrows
     public void run() {
-        while (distance < 100 && time < 21) {
+        while (time < 21) {
                 speed = (int) (Math.random() * 5);
                 Thread.sleep(1000);
                 distance += speed += time;
                 System.out.println(name + " has made it " + distance +
-                        "km and the speed was " + speed + "km per hour " + time);
+                        "km and the speed was " + speed + "km per hour ");
                 time++;
             }
         }
